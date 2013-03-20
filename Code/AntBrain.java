1e5d5be075681f6895c5e40ca7b41d5f216a3684
AFlight02 <alexflight@mac.com>
2013-03-20 14:11:17 +0000

    Added comments plus constructor

--- Code/AntBrain.java
+++ Code/AntBrain.java
@@ -4,16 +4,27 @@ import java.util.*;
 public class AntBrain {
 
 	//FSM brain; // need to find suitable FSM library/class
+	/* Alex: Perhaps we use a structure like this:
+	 * BufferedReader reader; // Reads in AntBrain text file like those in examples for project
+	 * FSMClassOfSomeKind fsm; // The Data structure of the fsm as read from the file
+	 */
+	
 	int gamesPlayed;
 	int gamesWon;
 	int state;
-
-	public void checkAntBrainSyntax() {
-
+	
+	public AntBrain() {
+		// Constructor stuff
 	}
 
-	public void returnInstruction() {
+	public void checkAntBrainSyntax(File brainFile) {
+		// Syntax analysis iterates through passed file line by line
+		// Valid syntax: COMMAND_TOKEN NEW_STATE etc.
+	}
 
+	public int returnInstruction() {
+		// Pass state to FSM, get the next state, update state attrib, return int representing instruction:
+		// 0 = wait, 1 = turn left, 2 = turn right, 3 = move, etc...
 	}
 
 }
