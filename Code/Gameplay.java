57ef8a963da82c8cc3cf113d045f1dc16a2296a9
AFlight02 <alexflight@mac.com>
2013-03-20 14:30:37 +0000

    Added method comments + constructor

--- Code/Gameplay.java
+++ Code/Gameplay.java
@@ -10,33 +10,37 @@ public class Gameplay {
 	Ant[] ants;
 	AntBrain redAndBrain;
 	AntBrain blackAntBrain;
-
-	public void generateWorld() {
-
+	
+	public Gameplay() {
+		// Constructor
 	}
 
-	public void loadWorld() {
+	public void generateWorld() {
+		// Generate a new World randomly
+	}
 
+	public void loadWorld(World uploadWorld) {
+		// Update world to an existing World that is passed
 	}
 
 	public void stepGame() {
-
+		// Go through all processes in a step of the game and repeat until all steps are done
 	}
 
 	public void endGame() {
-
+		// On completion of all steps finish the game, total scores etc.
 	}
 
 	public void setupGame() {
-
+		// Calls World syntax checks, AntBrain syntax checks, begins stepping through game instance
 	}
 
-	public void loadAntBrains() {
-
+	public void loadAntBrains(AntBrain red, AntBrain black) {
+		// Loads red and black AntBrains
 	}
 
-	public void declareWinner() {
-
+	public int declareWinner() {
+		// Calculates winners and returns 0 for Draw, 1 for Black win, 2 for Red win, -1 for error
 	}
 
 }
