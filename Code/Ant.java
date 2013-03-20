62819d592fd49171b1d40973f932fa4af466b6c6
AFlight02 <alexflight@mac.com>
2013-03-20 13:42:44 +0000

    Update Ant.java
    Added package declaration, example JavaDoc for class for us to follow

--- Code/Ant.java
+++ Code/Ant.java
@@ -1,3 +1,12 @@
+/**
+ * Ant.java
+ * Represents an instance of an individual Ant agent within the AntAttack simulation. Each Ant shares a common AntBrain
+ * that manages the behaviour of all Ants of a particualr team throughout a Tournament and individual Game
+ * 
+ * @author Software Engineering 2012-13 Group 9 - Simon Bell, Kirstie Hale, Paige Gray, Matt Chapman, Alex Flight (77525), ??James Bellamy??
+ * @version 0.1
+ */
+package AntAttack_Group9 // Need to combine everything into said package for portability and integration of classes together
 import java.util.*;
 
 public class Ant {
