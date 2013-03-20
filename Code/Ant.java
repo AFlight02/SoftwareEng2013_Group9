78fd983dec3222f7ade07199e129e96a29852a21
AFlight02 <alexflight@mac.com>
2013-03-20 13:59:48 +0000

    Commented Ant.Java with intentions

--- Code/Ant.java
+++ Code/Ant.java
@@ -10,42 +10,45 @@ package AntAttack_Group9 // Need to combine everything into said package for por
 import java.util.*;
 
 public class Ant {
-
-	boolean colour;
-	int state;
+	
 	AntBrain brain;
+	boolean colour;
+	boolean hasFood; // Missed an attribute in class dia...woops!
+	int state;
 	int direction;
 
 	public void getState() {
-
+		// Return state
 	}
 
-	public void changeDir() {
-
+	public void changeDir(int newDirection) {
+		// Update direction with passed new directon
 	}
 	
 	public void move() {
-
+		// Remove Ant from the Cell it currently resides in, add to the cell one cell in front of direction
 	}
 
 	public void pickUpFood() {
-
+		// Change hasFood to true
 	}
 
 	public void dropFood() {
-
+		// Change hasFood to false
 	}
 
-	public void markCell() {
-
+	public int markCell() {
+		// Return marker number to mark the current Cell
 	}
 
 	public void senseCell() {
-
+		// Get the attributes of cell in front, make call to change state based on what is sensed
 	}
 
-	public void updateCell() {
-
+	public void updateState() {
+		// Call to brain with current state as param
+		// state = brain.getState(state);
+		// Update state with result
 	}
 	
 }
