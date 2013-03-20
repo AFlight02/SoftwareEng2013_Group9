f3a2bc38bb54ad3caea08b601881d215ed09beb9
AFlight02 <alexflight@mac.com>
2013-03-20 14:03:07 +0000

    Added commented methods and constructor

--- Code/AntAttack.java
+++ Code/AntAttack.java
@@ -5,22 +5,26 @@ public class AntAttack {
 
 	GUI gui;
 	Tournament tournament;
-	int newAttr;
+	int newAttr; // Error in class diagram, will fix! Remove this on Friday.
+	
+	public AntAttack() {
+		// Constructor!
+	}
 
 	public void initialiseGUI() {
-
+		// Call to GUI constructor and initialisation methods.
 	}
 
 	public void newTournament() {
-
+		// Generate a new Tournment object
 	}
 
 	public void newGame() {
-
+		// Begin a new Game within the Tournament
 	}
 
 	public void newWorld() {
-
+		// Add new World to a Game within Tournament
 	}
 
 }
