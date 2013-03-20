6bbe0fdae9aa1d8c24ed29757b28e7a1eb00d212
AFlight02 <alexflight@mac.com>
2013-03-20 14:26:21 +0000

    Added commented methods + constructor

--- Code/GUI.java
+++ Code/GUI.java
@@ -4,33 +4,39 @@ import java.util.*;
 public class GUI {
 
 	int gameState;
+	// Need window elems plus way of representing each component in the game graphically in a window like an
+	// overhead map of some kind. See Google AI challenge as referenced in project for inspiration.
+	
+	public GUI() {
+		// New GUI object
+	}
 
 	public void beginNewTournament() {
-
+		// Call to AntAttack newTournament on button press
 	}
 
 	public void uploadAntBrain() {
-
+		// Again button press calls to upload an AntBrain
 	}
 
 	public void uploadWorld() {
-
+		// Button press calls AntAttack to add world
 	}
 
 	public void showWinner() {
-
+		// Call to show winner after game has finished
 	}
 
 	public void beginNewGame() {
-
+		// Begin a new game in AntAttack
 	}
 
 	public void initialiseUIElements() {
-
+		// Display all buttons, display, windows etc.
 	}
 
 	public void updateUI() {
-
+		// On game step(), update graphical display of the game world
 	}
 
 }
