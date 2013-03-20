b6f850487799ead20c525e4430c96b4b6c66883b
AFlight02 <alexflight@mac.com>
2013-03-20 14:50:12 +0000

    Commented methods + constructor

--- Code/World.java
+++ Code/World.java
@@ -5,37 +5,42 @@ public class World {
 
 	Cell[] cells;
 	int totalFood;
+	
+	public World() {
+		// Constructor
+	}
 
 	public void generateRandomWorld() {
-
+		// If call to this function, create a new well formed world of Cells[]
 	}
 
 	public void checkValidWorld() {
-
+		// Iterate through Cells, check each is as expected
 	}
 
-	public void findAnt() {
-
+	public int findAnt(int id) {
+		// Iterate through Cells for Ant of id, return cell index
 	}
 
-	public void checkCellStatus() {
-
+	public void checkCellStatus(int cell) {
+		// Return status of cell 
 	}
 
-	public void setFoodAtCell() {
-
+	public void setFoodAtCell(int cell) {
+		// Modify food at cell
 	}
 
-	public void setMarkAtCell() {
-
+	public void setMarkAtCell(int cell, int marker, bool colour) {
+		// Mark cell with number marker of colour where true = black, false = red (need to standardise the bool
+		// representation as concrete for the project as true always == black and false always == red!)
 	}
 
-	public void killAnt() {
-
+	public void killAnt(int antId) {
+		// Use findAnt(antId) and make call to clearAntFromCell(cellId). Call Gameplay to remove Ant from list of Ants
 	}
 
-	public void clearAntFromCell() {
-
+	public void clearAntFromCell(int cell) {
+		// Remove Ant from the cell
 	}
 
 }
