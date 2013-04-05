62ae9f5819b89a8bce24a9b131cb179ae3c7f9ad
sb542 <sb542@.ad.susx.ac.uk>
2013-04-05 12:46:55 +0100

    general changes to Cell

--- Code/AntAttack_Group9/Cell.java
+++ Code/AntAttack_Group9/Cell.java
@@ -4,112 +4,116 @@ import java.util.*;
 public class Cell {
 
 	Ant ant = null; // Ensure blank when no Ants exist
-        int[] pos = new int[2];
-        int food;
-        int adjacentAntsRed; // Counter updates each cycle, used to check if Ant dies in combat case
+    int[] pos = new int[2];
+    int food;
+    int adjacentAntsRed; // Counter updates each cycle, used to check if Ant dies in combat case
 	int adjacentAntsBlack;
 	boolean rock;
 	boolean[] markersRed; // Length 6 array, where marker num = i+1, structure is false, false, true, false etc..
 	boolean[] markersBlack;
 
 	public Cell(int x, int y) {
-            this.pos[0] = x;
-            this.pos[1] = y;
+        this.pos[0] = x;
+        this.pos[1] = y;
 	}
 	
-	public void setAnt() {
-		// Update Ant on this cell
+	public void setAnt(Ant ant) {
+		this.ant = ant;
+        // Update Ant on this cell
 	}
         
-        public int[] adjacentCell(int dir) {
-            int[] adjCell = new int[2];
-            switch(dir) {
-                case 0: 
+    public int[] adjacentCell(int dir) {
+        int[] adjCell = new int[2];
+        switch(dir) {
+            case 0: 
+                adjCell[0] = pos[0]++;
+                adjCell[1] = pos[1];
+                break;
+            case 1:
+                if(pos[1]%2 == 0) {
+                    adjCell[0] = pos[0];
+                    adjCell[1] = pos[1]++;
+                } else {
                     adjCell[0] = pos[0]++;
-                    adjCell[1] = pos[1];
-                    break;
-                case 1:
-                    if(pos[1]%2 == 0) {
-                        adjCell[0] = pos[0];
-                        adjCell[1] = pos[1]++;
-                    } else {
-                        adjCell[0] = pos[0]++;
-                        adjCell[1] = pos[1]++;
-                    }
-                    break;
-                case 2:
-                    if(pos[1]%2 == 0) {
-                        adjCell[0] = pos[0]--;
-                        adjCell[1] = pos[1]++;
-                    } else {
-                        adjCell[0] = pos[0];
-                        adjCell[1] = pos[1]++;
-                    }
-                    break;
-                case 3:
+                    adjCell[1] = pos[1]++;
+                }
+                break;
+            case 2:
+                if(pos[1]%2 == 0) {
                     adjCell[0] = pos[0]--;
-                    adjCell[1] = pos[1];
-                    break;
-                case 4:
-                    if(pos[1]%2 == 0) {
-                        adjCell[0] = pos[0]--;
-                        adjCell[1] = pos[1]--;
-                    } else {
-                        adjCell[0] = pos[0];
-                        adjCell[1] = pos[1]--;
-                    }
-                    break;
-                case 5:
-                    if(pos[1]%2 == 0) {
-                        adjCell[0] = pos[0];
-                        adjCell[1] = pos[1]--;
-                    } else {
-                        adjCell[0] = pos[0]++;
-                        adjCell[1] = pos[1]--;
-                    }
-                    break;
-            }
-            return adjCell;
+                    adjCell[1] = pos[1]++;
+                } else {
+                    adjCell[0] = pos[0];
+                    adjCell[1] = pos[1]++;
+                }
+                break;
+            case 3:
+                adjCell[0] = pos[0]--;
+                adjCell[1] = pos[1];
+                break;
+            case 4:
+                if(pos[1]%2 == 0) {
+                    adjCell[0] = pos[0]--;
+                    adjCell[1] = pos[1]--;
+                } else {
+                    adjCell[0] = pos[0];
+                    adjCell[1] = pos[1]--;
+                }
+                break;
+            case 5:
+                if(pos[1]%2 == 0) {
+                    adjCell[0] = pos[0];
+                    adjCell[1] = pos[1]--;
+                } else {
+                    adjCell[0] = pos[0]++;
+                    adjCell[1] = pos[1]--;
+                }
+                break;
         }
-        
-        public int[] sensedCell(int[] pos, int dir, Sense.senseDir sD) {
-            int[] sensedCellPos = new int[2];
-            switch(sD) {
-                case HERE:
-                    sensedCellPos = pos;
-                    break;
-                case AHEAD:
-                    sensedCellPos = adjacentCell(dir);
-                    break;
-                case LEFTAHEAD:
-                    sensedCellPos = adjacentCell(ant.turn(Turn.direction.LEFT));
-                    break;
-                case RIGHTAHEAD:
-                    sensedCellPos = adjacentCell(ant.turn(Turn.direction.RIGHT));
-                    break;
-            }
-            return sensedCellPos;
+        return adjCell;
+    }
+    
+    public int[] sensedCell(int[] pos, int dir, Sense.senseDir sD) {
+        int[] sensedCellPos = new int[2];
+        switch(sD) {
+            case HERE:
+                sensedCellPos = pos;
+                break;
+            case AHEAD:
+                sensedCellPos = adjacentCell(dir);
+                break;
+            case LEFTAHEAD:
+                sensedCellPos = adjacentCell(ant.turn(Turn.direction.LEFT));
+                break;
+            case RIGHTAHEAD:
+                sensedCellPos = adjacentCell(ant.turn(Turn.direction.RIGHT));
+                break;
         }
+        return sensedCellPos;
+    }
 
 	public int[] returnContents() {
 		// Returns attributes of this cell, may need to divide this into methods:
 		// bool getRock(){}
 		// int getFood(){}
 		// bool[] getMarkers(bool colour){}
-            int[] result = new int[1]; // Placeholder!
-            return result;
+        int[] result = new int[1]; // Placeholder!
+        return result;
 	}
 
 	public void removeAnt() {
 		// Set ant to null
+        this.ant = null;
 	}
 
 	public void addRedMarker(int markerNum) {
 		// Change markersRed[markerNum] to true
+        this.markersRed[markerNum] = true;
 	}
 
 	public void addBlackMarker(int markerNum) {
 		// As red
+        this.markersBlack[markerNum] = true;
 	}
 
 	public void calculateAdjacentAnts() {
