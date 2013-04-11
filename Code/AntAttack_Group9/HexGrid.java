d8ab5b7965126a76b33d3f07a68033869430b82d
Alex Flight <af255@sussex.ac.uk>
2013-04-11 19:40:15 +0100

    HexGrid optimisations

--- Code/AntAttack_Group9/HexGrid.java
+++ Code/AntAttack_Group9/HexGrid.java
@@ -84,7 +84,7 @@ public class HexGrid {
 
         @Override
         public void paintComponent(Graphics g) {
-            super.paintComponent(g);
+            //super.paintComponent(g);
             Graphics2D g2 = (Graphics2D) g;
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             g.setFont(new Font("TimesRoman", Font.PLAIN, 10));
@@ -210,8 +210,6 @@ public class HexGrid {
         }
         int y = i * (h - (t / 2));
         Polygon poly = hex(x, y);
-        g2.setColor(COLOURCELL);
-        g2.fillPolygon(poly);
         g2.setColor(COLOURGRID);
         g2.drawPolygon(poly);
     }
