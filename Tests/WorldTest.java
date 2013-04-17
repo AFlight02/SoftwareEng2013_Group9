e01ba81dccf1cadc2de711cdb66192c9498845d7
sb542 <simonspruzz@gmail.com>
2013-04-17 12:33:28 +0200

    Update WorldTest.java

--- Tests/WorldTest.java
+++ Tests/WorldTest.java
@@ -1,6 +1,7 @@
 package AntAttack_Group9;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -70,7 +71,8 @@ public class WorldTest {
     public void testCheckValidForTournament() throws Exception {
         System.out.println("checkValidForTournament");
         World instance = new World();
-        instance.generateRandomContestWorld();
+        instance.readInWorld("2.world");
+        //instance.generateRandomContestWorld();
         boolean expResult = true;
         boolean result = instance.checkValidForTournament();
         assertEquals(expResult, result);
@@ -90,13 +92,15 @@ public class WorldTest {
      * Test of checkCellStatus method, of class World.
      */
     @Test
-    public void testCheckCellStatus() {
+    public void testCheckCellStatus() throws IOException {
         System.out.println("checkCellStatus");
         int[] cell = new int[2];
         cell[0] = 1;
         cell[1] = 1;
         Sense.condition cond = Sense.condFromString("FRIEND");
-        Ant a = new Ant(false);
+        
+        AntBrain ab = new AntBrain("cleverbrain1.brain");
+        Ant a = new Ant(ab,false,0);
         World instance = new World();
         
         boolean expResult = false;
@@ -116,7 +120,9 @@ public class WorldTest {
         World instance = new World();
         instance.readInWorld("1.world");
         Cell expResult = new Cell(0,0);
-        expResult.setAnt(new Ant(false));
+        
+        AntBrain ab = new AntBrain("cleverbrain1.brain");
+        expResult.setAnt(new Ant(ab,false,0));
         Cell result = instance.getCell(cell);
         assertEquals(expResult.getFood(), result.getFood());
     }
@@ -128,25 +134,27 @@ public class WorldTest {
     public void testPrintWorld() {
         System.out.println("printWorld");
         World instance = new World();
+        instance.generateRandomContestWorld();
         instance.printWorld();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of placeAnts method, of class World.
      */
     @Test
-    public void testPlaceAnts() {
+    public void testPlaceAnts() throws IOException, Exception {
         System.out.println("placeAnts");
-        AntBrain blackBrain = null;
-        AntBrain redBrain = null;
+        AntBrain blackBrain = new AntBrain("cleverbrain1.brain");
+        AntBrain redBrain = new AntBrain("cleverbrain2.brain");
         World instance = new World();
-        List expResult = null;
+        instance.generateRandomContestWorld();
+        //instance.readInWorld("2.world");
+        List expResult = new ArrayList<Ant>();
         List result = instance.placeAnts(blackBrain, redBrain);
+        for(Object l:result){
+            System.out.println("HERE: "+l);
+        }
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -156,9 +164,12 @@ public class WorldTest {
     public void testReplaceFood() {
         System.out.println("replaceFood");
         World instance = new World();
+        instance.generateRandomContestWorld();
         instance.replaceFood();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        int expResult = 0;
+        int result = instance.getFoodNum();
+        assertEquals(expResult, result);
     }
 
     /**
@@ -168,11 +179,10 @@ public class WorldTest {
     public void testGetAnthillCells() {
         System.out.println("getAnthillCells");
         World instance = new World();
-        List expResult = null;
+        instance.generateRandomContestWorld();
+        List expResult = new ArrayList<>();
         List result = instance.getAnthillCells();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -182,11 +192,10 @@ public class WorldTest {
     public void testGetFoodCells() {
         System.out.println("getFoodCells");
         World instance = new World();
-        List expResult = null;
+        instance.generateRandomContestWorld();
+        List expResult = new ArrayList<>();
         List result = instance.getFoodCells();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -196,11 +205,10 @@ public class WorldTest {
     public void testGetFoodNum() {
         System.out.println("getFoodCells");
         World instance = new World();
+        instance.generateRandomContestWorld();
         int expResult = 0;
         int result = instance.getFoodNum();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -210,8 +218,7 @@ public class WorldTest {
     public void testResetWorld() {
         System.out.println("resetWorld");
         World instance = new World();
+        instance.generateRandomContestWorld();
         instance.resetWorld();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 }
